28ee591e90d385d1841b0e4db22798f5848b9e2f
EspoirMuhoza <ekucitini10@gmail.com>
2023-03-21 14:53:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - function that prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, y;
+
+	y = 0;
+
+	while (y < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{_putchar(n);
+		}
+		y++;
+		_putchar('\n');
+	}
+}
