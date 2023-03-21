a22f7bc61e6b30874038bdb86bb423a1261d81f0
EspoirMuhoza <ekucitini10@gmail.com>
2023-03-21 12:47:37 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- A program that prints _putchar
+ *
+ * Return: Always 0(Success)
+ */
+int main()
+{
+        puts("_putchar");
+        puts("\n");
+        return (0);
+}
+
