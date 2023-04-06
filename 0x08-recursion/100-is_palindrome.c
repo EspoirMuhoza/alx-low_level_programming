63804e83a982e1346e945d3445ce1b869c358a73
EspoirMuhoza <ekucitini10@gmail.com>
2023-04-06 04:05:49 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,12 +5,12 @@
  * @s: the string to count
  * Return: length of the string
  */
-int recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
 		s++;
-		return (1 + recursion(s));
+		return (1 + _strlen_recursion(s));
 	}
 	return (0);
 }
@@ -26,7 +26,7 @@ int checker(char *str, int len, int count)
 	if (count >= len)
 		return (1);
 	if (str[len] == str[count])
-		return (verify(str, len - 1, count + 1));
+		return (checker(str, len - 1, count + 1));
 	return (0);
 }
 /**
@@ -36,8 +36,8 @@ int checker(char *str, int len, int count)
  */
 int is_palindrome(char *s)
 {
-	int len = recursion(s);
+	int len = _strlen_recursion(s);
 	int count = 0;
 
-	return (verify(s, len - 1, count));
+	return (checker(s, len - 1, count));
 }
