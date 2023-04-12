a59f7b71481442534a82595db7954a3d5740273d
EspoirMuhoza <ekucitini10@gmail.com>
2023-04-12 15:46:47 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,65 +1,50 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * strtow - My function
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
- */
-char **strtow(char *str)
+  *argstostr - my function
+  *@ac: argument
+  *@av: pointer to array
+  *Return: NULL if ac == 0
+  *NULL on fail.
+  */
+char *argstostr(int ac, char **av)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	int a, b, c, size;
+	char *arg;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	size = 0;
+	c = 0;
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	a = 0;
+	while (a < ac)
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
-		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
-	{
-		count = 0;
-		if (str[wordf] != ' ')
+		b = 0;
+		while (av[a][b])
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
+			size++;
+			b++;
 		}
+		size++;
+		a++;
 	}
-	p[j] = NULL;
-	return (p);
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	a = 0;
+	while (a < ac)
+	{
+		b = 0;
+		while (av[a][b])
+		{
+			arg[c] = av[a][b];
+			b++;
+			c++;
+		}
+		arg[c] = '\n';
+		c++;
+		a++;
+	}
+	arg[c] = '\0';
+	return (arg);
 }
-
