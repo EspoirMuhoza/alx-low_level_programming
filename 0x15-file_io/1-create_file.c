65648c93a1a91261a60fce3d4cec6d7649ab0dc6
EspoirMuhoza <ekucitini10@gmail.com>
2023-05-08 04:16:33 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (~file)
+	if (file == -1)
 		return (-1);
 
 	write(file, text_content, espoir);
