57a061b852777fe1848bc706e4f706ff8cdfe6bd
EspoirMuhoza <ekucitini10@gmail.com>
2023-05-08 04:50:37 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,10 +43,10 @@ while (x == 1024)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
-if (close(fl_from == -1))
+if (close(fl_from) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fl_from), exit(100);
 
-if (close(fl_to == -1))
+if (close(fl_to ) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fl_to), exit(100);
 
 return (0);
